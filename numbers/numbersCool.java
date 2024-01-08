39cf5c8106e531dd77befad2ae9d3dfb376b2c2e
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2024-01-08 16:47:13 +0000

    heiqhdqh

---
+++ numbers/numbersCool.java
@@ -0,0 +1,13 @@
+class numbersCool
+{
+    public static void main (String args[])
+
+    {
+        int num1 = 2;
+        int num2 = 6;
+        int num3 = 1;
+        int num4 = 4;
+        int ans = (num1+num2+num3+num4) / 4;
+        System.out.println(ans);
+    }
+}
---
+++ numbers/numsTwo.java
@@ -0,0 +1,10 @@
+class numsTwo
+
+{
+    public static void main (String args[])
+    {
+        int num1 = 5;
+        int ans = (num1*num1) / 2;
+        System.out.println( ans);
+    }
+}
